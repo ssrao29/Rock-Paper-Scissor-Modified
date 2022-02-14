3abe1d806920ad782c5e32639f9dba89e98513f0
S Sindhura Rao <ssrao29@users.noreply.github.com>
2022-02-14 10:21:40 +0530

    Game Implementation

---
+++ RockPaperScissors_ModifiedGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private GameConfig _gameConfig;
+    
+    [SerializeField]
+    private GameScreenView _gameScreenView;
+
+    private IGameplayManager _gameplayManager;
+    private bool _isInitialised;
+    
+    private void Awake()
+    {
+        if(_gameConfig == null) Debug.LogError("No Game Config provided!");
+        
+        _gameplayManager = GameplayManager.Instance();
+        _gameplayManager.Init(_gameConfig);
+        _gameScreenView.Init(_gameConfig, _gameplayManager, QuitGame);
+        _isInitialised = true;
+    }
+
+    private void Start()
+    {
+        if (_isInitialised)
+        {
+            _gameScreenView.SetMenuScreen(_gameplayManager.SetGameplay);
+        }
+    }
+
+    private void Update()
+    {
+        if (_isInitialised)
+        {
+            var deltaTime = Time.deltaTime;
+            _gameplayManager.UpdateTimer(deltaTime);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _isInitialised = false;
+        _gameplayManager.Destroy();
+    }
+    
+    private void QuitGame()
+    {
+        _isInitialised = false;
+        Application.Quit();
+    }
+}
---
+++ RockPaperScissors_ModifiedGame/Assets/Scripts/View/HandView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandView : MonoBehaviour
+{
+    [SerializeField] private Text _name;
+    [SerializeField] private Image _icon;
+    [SerializeField] private Button _actionButton;
+
+    private int _handId;
+    
+    public void Init(HandInfo inHandInfo, Action<int> inClickCallback = null)
+    {
+        if (inHandInfo == null) return;
+        _handId = inHandInfo.Id;
+        SetName(inHandInfo.Name);
+        SetIcon(inHandInfo.Icon);
+        SetActionButton(inClickCallback);
+    }
+
+    private void SetName(string inName)
+    {
+        if (string.IsNullOrEmpty(inName) || _name == null) return;
+        _name.text = inName;
+    }
+    
+    private void SetIcon(Sprite inIconSprite)
+    {
+        if (inIconSprite == null || _icon == null) return;
+        _icon.sprite = inIconSprite;
+    }
+
+    private void SetActionButton(Action<int> inClickCallback)
+    {
+        if (_actionButton == null || inClickCallback == null) return;
+        _actionButton.onClick.AddListener(()=>
+        {
+            inClickCallback.Invoke(_handId);
+        });
+    }
+}
